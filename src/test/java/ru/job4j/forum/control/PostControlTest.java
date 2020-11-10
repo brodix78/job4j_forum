82449a8b4b106e1803da457a302c75d135e41666
brodix78 <brodnikov1950@gmail.com>
2020-11-10 20:55:25 +0400

    1. Spring mock[2096#304155]

--- src/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -1,20 +1,27 @@
 package ru.job4j.forum.control;
 
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -22,6 +29,9 @@ class PostControlTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private PostService posts;
 
     @Test
     @WithMockUser
@@ -35,6 +45,7 @@ class PostControlTest {
     @Test
     @WithMockUser
     public void idExistShouldReturnPostView() throws Exception {
+        Mockito.when(posts.findById(1)).thenReturn(new Post());
         MultiValueMap<String, String> paraMap =new LinkedMultiValueMap<>();
         paraMap.add("id", "1");
         this.mockMvc.perform(get("/post").params(paraMap))
@@ -46,6 +57,7 @@ class PostControlTest {
     @Test
     @WithMockUser
     public void idNotExistShouldReturnRedirect() throws Exception {
+        Mockito.when(posts.findById(1)).thenReturn(new Post());
         MultiValueMap<String, String> paraMap =new LinkedMultiValueMap<>();
         paraMap.add("id", "400");
         this.mockMvc.perform(get("/post").params(paraMap))
@@ -60,5 +72,35 @@ class PostControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("post/edit"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnRedirectAndSavePost() throws Exception {
+        this.mockMvc.perform(post("/post/save")
+                .param("name","Куплю машину")
+                .param("desc", "Куплю машину дорого"))
+                .andDo(print())
+                .andExpect(redirectedUrl("/"));
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(posts).save(argument.capture());
+        assertThat(argument.getValue().getName(), is("Куплю машину"));
+        assertThat(argument.getValue().getDesc(), is("Куплю машину дорого"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnRedirectAndUpdatePost() throws Exception {
+        this.mockMvc.perform(post("/post/update")
+                .param("id","10")
+                .param("name","Куплю машину")
+                .param("desc", "Куплю машину дорого"))
+                .andDo(print())
+                .andExpect(redirectedUrl("/"));
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(posts).update(argument.capture());
+        assertThat(argument.getValue().getId(), is(10));
+        assertThat(argument.getValue().getName(), is("Куплю машину"));
+        assertThat(argument.getValue().getDesc(), is("Куплю машину дорого"));
     }
 }
--- src/test/java/ru/job4j/forum/control/RegControlTest.java
+++ src/test/java/ru/job4j/forum/control/RegControlTest.java
@@ -1,20 +1,31 @@
 package ru.job4j.forum.control;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.User;
+import ru.job4j.forum.repository.UserRepository;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 class RegControlTest {
+
+    @MockBean
+    UserRepository users;
 
     @Autowired
     private MockMvc mockMvc;
@@ -26,5 +37,18 @@ class RegControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("reg"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnRedirectToLoginPageAndSaveUser() throws Exception {
+        this.mockMvc.perform(post("/reg")
+                .param("username","user")
+                .param("password", "1234"))
+                .andDo(print())
+                .andExpect(redirectedUrl("/login"));
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(users).save(argument.capture());
+        assertThat(argument.getValue().getUsername(), is("user"));
     }
 }
